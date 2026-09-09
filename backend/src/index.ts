09b80f0b17ee4e418f41f6b24423a76899ea374d
Zhuojun Xu <xuzhuojun1128@126.com>
2026-09-09 13:59:19 -0400

    Added for vercel deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://zhuojun-sass-dashboard.vercel.app",
+    ],
   }),
 );
 
