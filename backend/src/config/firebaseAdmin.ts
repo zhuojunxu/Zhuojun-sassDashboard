50c625c886eb9ebb51c6551d524f39368856ee16
Zhuojun Xu <xuzhuojun1128@126.com>
2026-09-09 14:15:37 -0400

    cleanup

--- backend/src/config/firebaseAdmin.ts
+++ backend/src/config/firebaseAdmin.ts
@@ -1,9 +1,4 @@
-import {
-  initializeApp,
-  cert,
-  getApps,
-  ServiceAccount,
-} from "firebase-admin/app";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
 
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
